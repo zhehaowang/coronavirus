1aeacb6729de58ed6bcaafa40889b999a87ced00
Zhehao Wang <wangzhehao410305@gmail.com>
2020-03-25 22:44:41 -0400

    amazon_refresh: add log to avoid self email spam

--- src/amazon_refresh.py
+++ src/amazon_refresh.py
@@ -4,7 +4,8 @@ import argparse
 import subprocess
 import smtplib
 import json
-
+import os
+import datetime
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -36,6 +37,11 @@ def parse_args():
         help="Sender email account credential file",
         default="../data/email.json",
     )
+    parser.add_argument(
+        "--log",
+        help="Log file to record status and avoid self spamming",
+        default="amazon_refresh.log",
+    )
     args = parser.parse_args()
     return args
 
@@ -55,9 +61,36 @@ class AmazonRefresh:
             self.sender = cred_obj["account"]
 
         self.recipient = args.email.split(",")
+        self.log = None
+        if args.log:
+            self.log = args.log
+        return
+
+    def should_act(self):
+        if not self.log or not os.path.isfile(self.log):
+            return True
+        with open(self.log, "r") as logfile:
+            lines = logfile.readlines()
+            if len(lines) > 0:
+                timestr, status = lines[-1].split(',')
+                time = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S.%f")
+                if status == "found" or status == "sorry":
+                    # 2 hours interval
+                    return (datetime.datetime.now() - time).total_seconds() < 3600 * 2
+        return True
+
+    def record_status(self, status):
+        if not self.log:
+            return
+        with open(self.log, "a") as logfile:
+            logfile.write("{},{}\n".format(datetime.datetime.now().isoformat(), status))
         return
 
     def request_amazon_refresh(self, target="Not available", target_num=3):
+        if not self.should_act():
+            print("sent a found email lately, skipped")
+            return
+
         process = subprocess.Popen(
             ["bash", self.curl_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
@@ -73,6 +106,7 @@ class AmazonRefresh:
                     "Amazon Refresh: adjust item",
                     "Something is no longer available :(\n\nSigh,\nBot",
                 )
+                self.record_status("sorry")
             else:
                 self.notify(
                     "Amazon Refresh: found availability",
@@ -80,8 +114,10 @@ class AmazonRefresh:
                         "https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1"
                     ),
                 )
+                self.record_status("found")
         else:
             print("occurrences {} == {}".format(target, target_num))
+            self.record_status("unavailable")
         return
 
     def notify(self, subject, text):
